a6abd8e76a62215123baa6d0edd9b2150cc776ad
Demo User 1 <demo1@example.com>
2026-07-07 21:40:55 +0800

    refactor: greet改为返回值模式

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        greet("World");
-        greet("世界");
-        greet("Git");
+        System.out.println(greet("World"));
+        System.out.println(greet("世界"));
+        System.out.println(greet("Git"));
     }
 
-    public static void greet(String name) {
-        System.out.println("Hello, " + name + "!");
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
     }
 }
